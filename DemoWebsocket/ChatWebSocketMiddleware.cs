cb4a41bb9354f264dcf284a6a83e61d07430e5f2
liuliang <liang.liu@ikangdental.com>
2019-01-24 11:37:36 +0800

    初始化

---
+++ DemoWebsocket/ChatWebSocketMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace demoWebsocket
+{
+    public class ChatWebSocketMiddleware
+    {
+        private static Dictionary<string, System.Net.WebSockets.WebSocket> _sockets = new Dictionary<string, System.Net.WebSockets.WebSocket>();
+
+        private readonly RequestDelegate _next;
+
+        public ChatWebSocketMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.WebSockets.IsWebSocketRequest)
+            {
+                await _next.Invoke(context);
+                return;
+            }
+            var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            string socketId = context.Request.Query["sid"].ToString();
+            if (!_sockets.ContainsKey(socketId))
+            {
+                _sockets.TryAdd(socketId, webSocket);
+            }
+
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+           
+        }
+    }
+}
---
+++ DemoWebsocket/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoWebsocket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
